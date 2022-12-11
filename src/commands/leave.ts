da131d823981af689b93e27a622d313d054c9188
Felipe Batista Alves <felipe.alves135@etec.sp.gov.br>
2022-12-11 02:23:51 -0300

    FEAT: comandos de leave e skip

---
+++ src/commands/leave.ts
@@ -0,0 +1,41 @@
+import { ColorResolvable, GuildMember, SlashCommandBuilder } from 'discord.js'
+import { CustomInteraction } from '../types/discord'
+import { getVoiceConnection } from '@discordjs/voice'
+import embedFactory from '../factory/embeds'
+import ServerService from '../services/server.service'
+
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('leave')
+    .setDescription('Deixa o canal de voz'),
+  async execute(
+    interaction: CustomInteraction,
+    user: GuildMember,
+    server: ServerService
+  ) {
+    const voiceChannel = user.voice.channel
+    const connection = getVoiceConnection(interaction.guild.id)
+    if (voiceChannel?.id !== connection?.joinConfig.channelId) {
+      return await interaction.reply({
+        content: 'Você precisa estar no mesmo canal de voz do bot para executar este comando!',
+        ephemeral: true
+      })
+    }
+    const leaveEmbed = embedFactory({
+      title: 'Desconectado',
+      botAvatar: server.getServerInfo(interaction.guildId).bot.avatarURL(),
+      botName: server.getServerInfo(interaction.guildId).bot.username,
+      description: 'Saindo do canal de voz',
+      color: process.env.BOT_COLOR as ColorResolvable,
+      footer: {
+        text: user.user.username,
+        iconUrl: user.user.avatarURL()
+      }
+    })
+
+    connection?.destroy()
+    server.clearQueue(interaction.guildId)
+    return await interaction.reply({ embeds: [leaveEmbed] })
+  }
+}
---
+++ src/commands/skip.ts
@@ -0,0 +1,24 @@
+import { GuildMember, SlashCommandBuilder } from 'discord.js'
+import { CustomInteraction } from '../types/discord'
+import { getVoiceConnection, VoiceConnectionReadyState } from '@discordjs/voice'
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('skip')
+    .setDescription('Pula a música atual'),
+  async execute(
+    interaction: CustomInteraction,
+    user: GuildMember
+  ) {
+    const voiceChannel = user.voice.channel
+    const connection = getVoiceConnection(interaction.guild.id)
+    if (voiceChannel?.id !== connection?.joinConfig.channelId) {
+      return await interaction.reply({
+        content: 'Você precisa estar no mesmo canal de voz do bot para executar este comando!',
+        ephemeral: true
+      })
+    }
+    (connection?.state as VoiceConnectionReadyState)?.subscription?.player.stop()
+    return await interaction.reply({ content: 'Pulando a música atual' })
+  }
+}
