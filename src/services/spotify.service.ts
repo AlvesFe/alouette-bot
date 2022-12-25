2ff8509eed90a3ee539538f05db6c2ecb22d35b3
Felipe Batista Alves <felipe.alves135@etec.sp.gov.br>
2022-12-25 02:21:38 -0300

    FEAT:adiciona get de token de acesso via app

--- src/services/spotify.service.ts
+++ src/services/spotify.service.ts
@@ -4,12 +4,18 @@ import ytbService from './ytb.service'
 const { SPOTIFY_CLIENT_ID, SPOTIFY_SECRET } = process.env
 
 class SpotifyService {
-  client: Spotify
+  client = new Spotify({
+    clientId: SPOTIFY_CLIENT_ID,
+    clientSecret: SPOTIFY_SECRET
+  })
+
   constructor() {
-    this.client = new Spotify({
-      clientId: SPOTIFY_CLIENT_ID,
-      clientSecret: SPOTIFY_SECRET
-    })
+    void this.auth()
+  }
+
+  async auth(): Promise<void> {
+    const data = await this.client.clientCredentialsGrant()
+    this.client.setAccessToken(data.body.access_token)
   }
 
   async search(query: string): Promise<SearchResults> {
