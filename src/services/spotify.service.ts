aa40e09448d1342da178bde219a22a0f623ea14f
Felipe Batista Alves <lipe.batistao@live.com>
2023-01-14 19:21:44 -0300

    FIX: reseta o token de acesso para cada chamada de API

--- src/services/spotify.service.ts
+++ src/services/spotify.service.ts
@@ -7,41 +7,43 @@ class SpotifyService {
     clientSecret: SPOTIFY_SECRET
   })
 
-  constructor() {
-    void this.auth()
-  }
-
   async auth(): Promise<void> {
     const data = await this.client.clientCredentialsGrant()
     this.client.setAccessToken(data.body.access_token)
   }
 
   async searchTrack(query: string): Promise<SpotifyApi.TrackObjectFull | null> {
+    await this.auth()
     const data = await this.client.searchTracks(query)
     return data?.body.tracks?.items[0] || null
   }
 
   async getTrackInfo(trackId: string): Promise<SpotifyApi.TrackObjectFull> {
+    await this.auth()
     const track = await this.client.getTrack(trackId)
     return track.body
   }
 
   async getPlaylistInfo(playlistId: string): Promise<SpotifyApi.PlaylistTrackObject[]> {
+    await this.auth()
     const playlist = await this.client.getPlaylist(playlistId)
     return playlist.body.tracks.items
   }
 
   async getEpisodeInfo(episodeId: string): Promise<SpotifyApi.EpisodeObjectFull> {
+    await this.auth()
     const episode = await this.client.getEpisode(episodeId)
     return episode.body
   }
 
   async getAlbumInfo(albumId: string): Promise<SpotifyApi.TrackObjectSimplified[]> {
+    await this.auth()
     const album = await this.client.getAlbum(albumId)
     return album.body.tracks.items
   }
 
   async getArtistTracks(artistId: string): Promise<SpotifyApi.TrackObjectFull[]> {
+    await this.auth()
     const artist = await this.client.getArtistTopTracks(artistId, 'US')
     return artist.body.tracks
   }
