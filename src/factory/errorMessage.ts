ac77d2ec0c02e0a5004cc61148b2cc478bee0e76
Felipe Batista Alves <lipe.batistao@live.com>
2023-01-14 20:44:45 -0300

    FIX: melhor tratativa de logs de erro

--- src/factory/errorMessage.ts
+++ src/factory/errorMessage.ts
@@ -1,25 +1,28 @@
 import { ErrorFactory } from '../types/error'
 import * as fs from 'fs'
+import path from 'path'
 
 const errorMetric = ({
   message,
   error,
   data
 }: ErrorFactory): void => {
-  const timestamp = new Date().toISOString()
+  const timestamp = new Date()
   const payload = {
-    timestamp,
+    timestamp: timestamp.toISOString(),
     message,
-    error,
+    error: String(error),
     data
   }
-  if (!fs.existsSync('./logs')) {
-    fs.mkdirSync('./logs')
+
+  const logsPath = path.join(__dirname, '../../logs')
+
+  if (!fs.existsSync(logsPath)) {
+    fs.mkdirSync(logsPath)
   }
-  fs.writeFileSync(
-    `./logs/error-${timestamp}.json`,
-    JSON.stringify(payload, null, 2)
-  )
+
+  const logFile = path.join(logsPath, `error-${timestamp.getTime()}.log`)
+  fs.writeFileSync(logFile, JSON.stringify(payload, null, 2))
   console.error(payload)
 }
 
