e8cf780e2bbc0bda684d52cbc4bfba9560c9b3e5
Felipe Batista Alves <lipe.batistao@live.com>
2023-02-13 23:33:52 -0300

    FIX: limpar a fila na disconex~ao automática

--- src/events/onDisconnection.ts
+++ src/events/onDisconnection.ts
@@ -6,7 +6,7 @@ import { CustomClient } from '../types/discord'
 export default {
   name: 'voiceStateUpdate',
   once: false,
-  async execute(oldState: VoiceState, newState: VoiceState, _server: ServerService, client: CustomClient) {
+  async execute(oldState: VoiceState, newState: VoiceState, server: ServerService, client: CustomClient) {
     const channelId = newState.channelId || oldState.channelId
     if (!channelId) return
     const channel = await client.channels.fetch(channelId) as VoiceBasedChannel
@@ -15,6 +15,7 @@ export default {
     if (membersInVoiceChannel === 1 && isBotInVoiceChannel) {
       setTimeout(() => {
         const connection = getVoiceConnection(newState.guild.id)
+        server.clearQueue(newState.guild.id)
         connection?.destroy()
       }, 5000)
     }
